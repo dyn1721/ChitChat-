917e497a21d10578fbd702eee08fec57170c0a90
dyn1721 <qqduanyuning@163.com>
2020-06-12 21:56:44 +0800

    登录cookie

--- js/cookie.js
+++ js/cookie.js
@@ -1,26 +1,24 @@
-/*设置cookie*/
-export function setCookie(c_name,value,expire) {
-    var date=new Date()
-    date.setSeconds(date.getSeconds()+expire)
-    document.cookie=c_name+ "="+escape(value)+"; expires="+date.toGMTString()
-    console.log(document.cookie)
-}
-
-/*获取cookie*/
-export function getCookie(c_name){
-    if (document.cookie.length>0){
-        let c_start=document.cookie.indexOf(c_name + "=")
-        if (c_start!=-1){ 
-            c_start=c_start + c_name.length+1 
-            let c_end=document.cookie.indexOf(";",c_start)
-            if (c_end==-1) c_end=document.cookie.length
-                return unescape(document.cookie.substring(c_start,c_end))
-            } 
-        }
-    return ""
-}
-
-/*删除cookie*/
-export function delCookie(c_name){
-    setCookie(c_name, "", -1)
-}
+function delCookie()
+			{
+			 var keys=document.cookie.match(/[^ =;]+(?=\=)/g);
+			    if (keys) {
+			        for (var i =  keys.length; i--;)
+			            document.cookie=keys[i]+'=0;expires=' + new Date( 0).toUTCString()
+			    }    
+			}
+function setCookie(cname,cvalue,exdays){
+				var d = new Date();
+				d.setTime(d.getTime()+(exdays*24*60*60*1000));
+				var expires = "expires="+d.toGMTString();
+				document.cookie = cname+"="+cvalue+"; "+expires;
+				console.log(document.cookie)
+			}
+function getCookie(cname){
+				var name = cname + "=";
+				var ca = document.cookie.split(';');
+				for(var i=0; i<ca.length; i++) {
+					var c = ca[i].trim();
+					if (c.indexOf(name)==0) { return c.substring(name.length,c.length); }
+				}
+				return "";
+			}
