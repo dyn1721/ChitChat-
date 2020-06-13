fdc681cdc699db57ce0d84b625de3f15800c07de
dyn1721 <qqduanyuning@163.com>
2020-06-13 20:11:46 +0800

    设置模块模子 注销功能

--- js/cookie.js
+++ js/cookie.js
@@ -2,15 +2,18 @@ function delCookie()
 			{
 			 var keys=document.cookie.match(/[^ =;]+(?=\=)/g);
 			    if (keys) {
-			        for (var i =  keys.length; i--;)
-			            document.cookie=keys[i]+'=0;expires=' + new Date( 0).toUTCString()
-			    }    
+					console.log(1)
+			        for (var i =  keys.length; i--;){
+						console.log(document.cookie)
+			            document.cookie=keys[i]+'=0;expires=' + new Date( 0).toUTCString()+";path=/ChitChat-"}
+			    } 
+				   console.log( document.cookie)
 			}
 function setCookie(cname,cvalue,exdays){
 				var d = new Date();
 				d.setTime(d.getTime()+(exdays*24*60*60*1000));
 				var expires = "expires="+d.toGMTString();
-				document.cookie = cname+"="+cvalue+"; "+expires;
+				document.cookie = cname+"="+cvalue+"; "+expires+"; path=/ChitChat-";
 				console.log(document.cookie)
 			}
 function getCookie(cname){
